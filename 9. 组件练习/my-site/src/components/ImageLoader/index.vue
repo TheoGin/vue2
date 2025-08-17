65c7a64f0beb9d1ac50daac93cc9f5101d02accd
Sophotheo <2559629448@qq.com>
2025-08-17 22:20:36 +0800

    feat: 9.5 ImageLoader

---
+++ 9. 组件练习/my-site/src/components/ImageLoader/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="image-loader-container">
+    <img v-if="loadingFinish" :src="src" alt="" />
+    <img v-else :src="placeholder" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      default: 500,
+    },
+  },
+  data() {
+    return {
+      loadingFinish: false,
+    };
+  },
+  onMounted(){
+    console.log(111);
+    
+    setTimeout(() => {
+        this.loadingFinish = true;
+    }, this.duration);
+  },
+};
+</script>
+
+<style scoped lang="less">
+.image-loader-container {
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    display: block;
+  }
+}
+</style>
---
+++ 9. 组件练习/my-site/src/components/ImageLoader/test.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="test-container">
+    <ImgageLoader placeholder="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?w=100" src="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?fit=crop&crop=entropy&w=3456&h=2304" />
+  </div>
+</template>
+
+<script>
+import ImgageLoader from "./";
+
+export default {
+  components: {
+    ImgageLoader,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="less">
+.test-container {
+  position: relative;
+  border: 1px solid;
+  width: 500px;
+  height: 400px;
+  margin: 0 auto;
+}
+</style>
