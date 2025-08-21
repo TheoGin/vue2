c8333af35f48384172e2444d96c6146672d87404
傅朝俊 <fuchaojun@szkingdom.com>
2025-08-21 21:47:28 +0800

    feat：Message组件

---
+++ 10. 插槽/Message.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="message-container">
+    <div class="content">abc</div>
+    <button>确定</button>
+    <button>关闭</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ 10. 插槽/MessageTest.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="test-container">
+    <Message  />
+    
+    <!-- <Message msg="abc" /> -->
+    <!-- 传授的信息有限 -> 插槽 -->
+     <slot></slot>
+  </div>
+</template>
+
+<script>
+import Message from './Message.vue';
+
+export default {
+    components: {
+        Message
+    }
+}
+</script>
+
+<style>
+
+</style>
