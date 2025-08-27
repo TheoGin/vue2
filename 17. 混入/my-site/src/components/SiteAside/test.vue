2d395c4fccc3f719a86421934b14f84403b9fee4
傅朝俊 <fuchaojun@szkingdom.com>
2025-08-27 22:49:39 +0800

    init 17. 混入

---
+++ 17. 混入/my-site/src/components/SiteAside/test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="test-container">
+    <SiteAside />
+  </div>
+</template>
+
+<script>
+import SiteAside from "./";
+import "@/styles/global.less";
+
+export default {
+  components: {
+    SiteAside,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+@import "~@/styles/var.less";
+
+.test-container {
+  border: 1px solid;
+  width: 250px;
+  height: 600px;
+  margin: 0 auto;
+}
+</style>
