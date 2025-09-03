9af13eeee54aeef82d33eddbd76c9d595a21c534
傅朝俊 <fuchaojun@szkingdom.com>
2025-09-03 23:01:09 +0800

    feat: 21.2 $listeners

---
+++ 21. $listeners和v-model/demo1/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <LoadingButton @click="handleClick"/>
+</template>
+
+<script>
+import LoadingButton from "./LoadingButton";
+
+export default {
+  components: {
+    LoadingButton,
+  },
+  methods: {
+    async handleClick(count) {
+      console.log('父组件', count)
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve('失败了')
+        }, 1000)
+      })
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  text-align: center;
+}
+</style>
