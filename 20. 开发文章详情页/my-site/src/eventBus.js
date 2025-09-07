a0adb40e958578eeabdca6b3365942bbf7f6c5da
傅朝俊 <fuchaojun@szkingdom.com>
2025-09-07 22:59:45 +0800

    feat: 22. 事件总线

---
+++ 20. 开发文章详情页/my-site/src/eventBus.js
@@ -0,0 +1,47 @@
+// // { event1: [handler1, handler2], event2: [handler2] }
+// const listeners = {};
+
+// // 事件总线
+// export default {
+//   /**
+//    * 监听某一个事件
+//    * @param eventName 事件名称
+//    * @param handler 处理函数
+//    */
+//   $on(eventName, handler) {
+//     if (!listeners[eventName]) {
+//       // 使用 Set 集合去重
+//       listeners[eventName] = new Set();
+//     }
+//     listeners[eventName].add(handler);
+//   },
+//     /**
+//    * 取消监听
+//    * @param eventName 事件名称
+//    * @param handler 处理函数
+//    */
+//   $off(eventName, handler) {
+//     if (!listeners[eventName]) {
+//       return;
+//     }
+//     listeners[eventName].delete(handler);
+//   },
+//     /**
+//    * 触发事件
+//    * @param eventName 事件名称
+//    * @param args 参数
+//    */
+//   $emit(eventName, ...args) {
+//     if (!listeners[eventName]) {
+//       return;
+//     }
+//     for (const handler of listeners[eventName]) {
+//       handler(...args);
+//     }
+//   },
+// };
+
+import Vue from 'vue';
+
+export default new Vue({});
+
--- 20. 开发文章详情页/my-site/src/main.js
+++ 20. 开发文章详情页/my-site/src/main.js
@@ -8,7 +8,7 @@ import VLoading from './directives/loading.js';
 
 Vue.directive('loading', VLoading);
 
-/* 
+/*
 window.showMessage = showMessage;
 // showMessage("abc", "success");
 showMessage({
@@ -27,3 +27,38 @@ new Vue({
   router,
 }).$mount("#app");
 
+/* import eventBus from "@/eventBus";
+
+function handler1(data) {
+  console.log("handler1", data);
+}
+
+function handler2(data) {
+  console.log("hander2", data);
+}
+
+window.eventBus = eventBus;
+window.handler1 = handler1;
+window.handler2 = handler2;
+
+eventBus.$on("event1", handler1);
+eventBus.$on("event1", handler2);
+eventBus.$on("event2", handler1);
+
+eventBus.$emit("event1", 123); // handler1 123  hander2 123
+eventBus.$emit("event2", 123); // handler1 123
+
+eventBus.$off("event1", handler1);
+eventBus.$emit("event1", 123); // hander2 123
+
+eventBus.$emit('event1', 123) // handler1 123 hander2 123
+// Vue {_uid: 0, _isVue: true, $options: {…}, _renderProxy: Proxy(Vue), _self: Vue, …}
+
+eventBus.$emit('event2', 123) // handler1 123
+// Vue {_uid: 0, _isVue: true, $options: {…}, _renderProxy: Proxy(Vue), _self: Vue, …}
+
+eventBus.$off('event1', handler1)
+// Vue {_uid: 0, _isVue: true, $options: {…}, _renderProxy: Proxy(Vue), _self: Vue, …}
+
+eventBus.$emit('event1', 123) // hander2 123
+// Vue {_uid: 0, _isVue: true, $options: {…}, _renderProxy: Proxy(Vue), _self: Vue, …} */
