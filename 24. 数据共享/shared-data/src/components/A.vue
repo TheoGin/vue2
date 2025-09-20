13256cdb8c115bd21c97ced7b2b30025edabd358
傅朝俊 <fuchaojun@szkingdom.com>
2025-09-20 14:01:49 +0800

    init 24. 数据共享

---
+++ 24. 数据共享/shared-data/src/components/A.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="block">
+    <p>the number is</p>
+    <h1>0</h1>
+    <p>
+      <button>减少</button>
+      <button>异步增加</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+  },
+};
+</script>
