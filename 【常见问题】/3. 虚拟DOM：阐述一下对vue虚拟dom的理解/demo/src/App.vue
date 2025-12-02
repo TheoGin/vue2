e643345b3a4a8ae9756c04ace4dae513ce5729b9
傅朝俊 <fuchaojun@szkingdom.com>
2025-12-02 22:46:42 +0800

    feat：3. 虚拟DOM：阐述一下对vue虚拟dom的理解

---
+++ 【常见问题】/3. 虚拟DOM：阐述一下对vue虚拟dom的理解/demo/src/App.vue
@@ -0,0 +1,46 @@
+<!--<template>
+  &lt;!&ndash;<div id="app" a="1" b="2">
+    <img
+      alt="Vue logo"
+      src="https://avatars.githubusercontent.com/u/98071271?v=4"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>&ndash;&gt;
+</template>-->
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  template: `
+    <div>
+      <h1>title1</h1>
+      <h2>title2</h2>
+    </div>
+  `,
+  mounted() {
+    console.log("App", this._vnode);
+  },
+  /*render(h) {
+    return h("div", [
+      h("h1", 'title1'),
+      h("h2", 'title2'),
+    ]);
+  },*/
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 【常见问题】/3. 虚拟DOM：阐述一下对vue虚拟dom的理解/demo/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: "",
+    },
+  },
+  created() {
+    console.log('HelloWorld', this);
+  }
+};
+</script>
+
+<style scoped></style>
