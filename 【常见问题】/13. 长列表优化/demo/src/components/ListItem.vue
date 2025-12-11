966006da97df014391d3d435c9e14ab99b889f6d
傅朝俊 <fuchaojun@szkingdom.com>
2025-12-11 22:41:54 +0800

    feat：13.1 不使用虚拟滚动：可能会卡死

---
+++ 【常见问题】/13. 长列表优化/demo/src/components/ListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="item-container">
+    <div>id{{ item.id }}</div>
+    <div>name{{ item.name }}</div>
+    <div>age{{ item.age }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: []
+    }
+  }
+};
+</script>
+
+<style scoped>
+.item-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  border-bottom: 1px solid #ccc;
+  height: 54px;
+  line-height: 54px;
+}
+</style>
