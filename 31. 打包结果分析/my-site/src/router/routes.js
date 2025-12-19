32d7d31c15a10bb9c82c3b1bdbc83452a1f80fc5
傅朝俊 <fuchaojun@szkingdom.com>
2025-12-19 23:04:16 +0800

    feat: 32. 异步组件

--- 31. 打包结果分析/my-site/src/router/routes.js
+++ 31. 打包结果分析/my-site/src/router/routes.js
@@ -4,12 +4,49 @@ import Blog from "@/views/Blog";
 import Message from "@/views/Message";
 import Project from "@/views/Project";
 import Detail from "@/views/Blog/Detail.vue";*/
+import { start, done, configure } from 'nprogress'
+import 'nprogress/nprogress.css'
+
+configure({
+  showSpinner: false,
+  trickleSpeed: 20,
+})
+
+// window.start = start
+// window.done = done
+
+function delay(duration = 1000) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, duration);
+  });
+}
+
+/**
+ * 不能传递 path 或 import(...)，不然会当做表达式，分包就无效了
+ * @param { Function } pageCompResolver
+ */
+// async function getAsyncComponent(pageCompResolver) { // 这里不需要写 async
+function getAsyncComponent(pageCompResolver) {
+  return async () => {
+    if (process.env.NODE_ENV === 'development') {
+      await delay();
+    }
+    // console.log("加载前");
+    start();
+    const comp = await pageCompResolver();
+    // console.log("加载后");
+    done()
+    return comp;
+  }
+}
 
 export default [
   {
     name: "Home",
     path: "/",
-    component: () => import(/* webpackChunkName: "Home" */ "@/views/Home"),
+    component: getAsyncComponent(() => import(/* webpackChunkName: "Home" */ "@/views/Home")),
     meta: {
       title: "首页",
     },
@@ -17,7 +54,7 @@ export default [
   {
     name: "About",
     path: "/about",
-    component: () => import(/* webpackChunkName: "About" */ "@/views/About"),
+    component: getAsyncComponent(() => import(/* webpackChunkName: "About" */ "@/views/About")),
     meta: {
       title: "关于我",
     },
@@ -26,7 +63,7 @@ export default [
     // 假如要更改路径，还要改RouterLink，还可能要改其他地方 --> 命名路由
     name: "Blog",
     path: "/article",
-    component: () => import(/* webpackChunkName: "Blog" */ "@/views/Blog"),
+    component: getAsyncComponent(() => import(/* webpackChunkName: "Blog" */ "@/views/Blog")),
     meta: {
       title: "文章",
     },
@@ -34,7 +71,7 @@ export default [
   {
     name: "BlogDetail",
     path: "/article/:id",
-    component: () => import(/* webpackChunkName: "BlogDetail" */ "@/views/Blog/Detail.vue"),
+    component: getAsyncComponent(() => import(/* webpackChunkName: "BlogDetail" */ "@/views/Blog/Detail.vue")),
     meta: {
       title: "文章详情",
     },
@@ -42,7 +79,7 @@ export default [
   {
     name: "BlogCategory",
     path: "/article/cate/:categoryId",
-    component: () => import(/* webpackChunkName: "Blog" */ "@/views/Blog"),
+    component: getAsyncComponent(() => import(/* webpackChunkName: "Blog" */ "@/views/Blog")),
     meta: {
       title: "文章",
     },
@@ -50,7 +87,7 @@ export default [
   {
     name: "Message",
     path: "/message",
-    component: () => import(/* webpackChunkName: "Message" */ "@/views/Message"),
+    component: getAsyncComponent(() => import(/* webpackChunkName: "Message" */ "@/views/Message")),
     meta: {
       title: "留言板",
     },
@@ -58,7 +95,7 @@ export default [
   {
     name: "Project",
     path: "/project",
-    component: () => import(/* webpackChunkName: "Project" */ "@/views/Project"),
+    component: getAsyncComponent(() => import(/* webpackChunkName: "Project" */ "@/views/Project")),
     meta: {
       title: "项目&效果",
     },
